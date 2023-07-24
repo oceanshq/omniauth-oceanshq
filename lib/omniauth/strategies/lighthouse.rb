a3ddc89a467627e68e51231fc147de69e5a02f3e
Vitalii <vitaliy@cloudassess.com.au>
2023-07-24 20:21:07 +1000

    fixed inherittens

--- lib/omniauth/strategies/lighthouse.rb
+++ lib/omniauth/strategies/lighthouse.rb
@@ -2,7 +2,7 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
-    class Vhq < Lighthouse
+    class Lighthouse < Oceanshq
       # Request access to the lighthouse API
       option :authorize_params, { scope: 'lighthouse' }
     end
